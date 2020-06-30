5dae8ffbedb7a8ca82708e94d64e4af004885cb6
Irek <irek140@poczta.fm>
2020-06-30 16:00:45 +0200

    Dodanie obsługi podziału szyfrowanego tekstu na kraty zawierające po maks 36 znaków

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 # Program realizujący szyfrowanie i deszyfrowanie przy użyciu szyfru grilla Cardano
-
+from math import ceil # funkcja odpowiedzialna za zaokrąglanie w górę
 
 # Funkcja rozpoczynająca działanie menu programu
 def start_program():
@@ -19,9 +19,33 @@ def start_program():
 def encryption():
     print("Wpisz wiadomość, która ma zostać zaszyfrowana szyfrem grilla Cardano: ")
     message = input()
+
+    message_char_list = list(message) # Zamiana wprowadzonego tekstu na listę znaków
+
+    # Implementacja algorytmu szyfrującego
+    krypto_tab = []
+    number_of_grills = (ceil(len(message_char_list) / 36)) # Wyznaczenie liczby krat(grilli)
+    grills = [] # Tablica przechowująca fragmenty wiadomości mieszczące się na kratach
+    for grille in range(number_of_grills): # Podział wiadomości na kraty zawierające 36 znaków
+        grill = []
+        for i in range(36):
+            if(len(message_char_list) > 0):
+                print("Iteracja: "); print(i)
+                print("Grill: "); print(grill)
+                print("Litera do grilla: "); print(message_char_list[0])
+                grill.append(message_char_list[0])
+                message_char_list.pop(0)
+                print("Wiadomość: "); print(message_char_list)
+                print("###################################################")
+        grills.append(grill)
+        print(grill)
+    print(grills)
+
+
+
     try:
         file = open('kryptotekst.txt', 'w')
-        file.write(message) # Zapis do pliku
+        file.write(message) # Zapis zaszyfrowanej wiadomości do pliku
     finally:
         file.close()
 
